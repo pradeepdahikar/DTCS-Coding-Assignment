4c5067df8b8f7cb7eb733a0694fe158473a5b1ec
Pradeep_Dahikar <pradeep.dahikar@gmail.com>
2020-09-14 15:27:58 +0530

    Unit Testing Added(xUniy woth Moq)

---
+++ PatientRegistration.Test/LocationServiceUnitTest.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Patient.Model.LocationModule;
+using Patient.Service.Interfaces;
+using Patient_Registration.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PatientRegistration.Test
+{
+    [TestClass]
+    public class LocationServiceUnitTest
+    {
+        ILocationService _locationService;
+
+        public LocationServiceUnitTest()
+        {
+            SetUpMocks();
+        }
+
+        private void SetUpMocks()
+        {
+            var mockRepo = new Mock<ILocationService>();
+
+            IList<StateModel> stateList = new List<StateModel>()
+            {
+            new StateModel {
+                Id=1,
+                Name="MH"
+            }
+            };
+
+            IList<CityModel> cityList = new List<CityModel>()
+            {
+            new CityModel {
+                Id=1,
+                Name="Pune",
+            }
+            };
+
+
+            mockRepo
+                .Setup(repo => repo.GetStateListAsync())
+                .ReturnsAsync(stateList.ToList());
+
+            mockRepo
+                .Setup(repo => repo.GetCityListByStateIdAsync(1))
+                .ReturnsAsync(cityList.ToList());
+
+            mockRepo.SetupAllProperties();
+
+            _locationService = mockRepo.Object;
+        }
+
+        [TestMethod]
+        public void GetStateList()
+        {
+            //Arrange the resources
+            var controlller = new LocationController(_locationService);
+
+            //Act on functionality
+            var stateList = controlller.GetStates().Result;
+            var okResult = stateList as OkObjectResult;
+            var actualStateList = (List<StateModel>)okResult.Value;
+
+            //Assert The result against expected
+            Assert.IsTrue(actualStateList[0].Name != string.Empty);
+        }
+
+        [TestMethod]
+        public void GetCityListByStateId()
+        {
+            //Arrange the resources
+            var controlller = new LocationController(_locationService);
+
+            //Act on functionality
+            var cityList = controlller.GetCityById(1).Result;
+            var okResult = cityList as OkObjectResult;
+            var actualCityList = (List<CityModel>)okResult.Value;
+
+            //Assert The result against expected
+            Assert.IsTrue(actualCityList[0].Name != string.Empty);
+        }
+    }
+}
---
+++ PatientRegistration.Test/PatientServiceUnitTest.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Patient.Model;
+using Patient.Service.Interfaces;
+using Patient.Service.Services;
+using Patient_Registration.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace PatientRegistration.Test
+{
+    [TestClass]
+    public class PatientServiceUnitTest
+    {
+        IPatientService _patientService;
+
+        public PatientServiceUnitTest()
+        {
+            SetUpMocks();
+        }
+
+        private void SetUpMocks()
+        {
+            var mockRepo = new Mock<IPatientService>();
+
+            IList<PatientModel> patientDetails = new List<PatientModel>()
+            {
+            new PatientModel {
+                Name="Venkat",
+                SurName="Swami",
+                CityId=1,
+                DOB=DateTime.Now,
+                Gender="M"
+            }
+            };
+
+            mockRepo
+                .Setup(repo => repo.GetPatientListAsync())
+                .ReturnsAsync(patientDetails.ToList());
+
+            mockRepo.SetupAllProperties();
+
+            _patientService = mockRepo.Object;
+        }
+
+        [TestMethod]
+        public void GetPatientsDetails_AllPatients()
+        {
+            //Arrange the resources
+            var controlller = new PatientController(_patientService);
+
+            //Act on functionality
+            var patientsList = controlller.Get().Result;
+            var okResult = patientsList as OkObjectResult;
+            var actualPatientList = (List<PatientModel>)okResult.Value;
+
+            //Assert The result against expected
+            Assert.IsTrue(actualPatientList[0].Name!=string.Empty);
+        }
+
+        [TestMethod]
+        public void RegisterPatient()
+        {
+            var controller = new PatientController(_patientService);
+
+            var patientsList = controller.Get().Result;
+            var okResult = patientsList as OkObjectResult;
+            var actualPatientList = (List<PatientModel>)okResult.Value;
+
+            var result = controller.Post(actualPatientList[0]).Result;
+            var resultFromPOST = (bool)((OkObjectResult)result).Value;
+          
+
+            Assert.IsTrue(resultFromPOST);
+        }
+
+    }
+}
